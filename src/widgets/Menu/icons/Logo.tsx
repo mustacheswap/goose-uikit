6389d26c06a1f2ae890c9ac626e1f122a1b11a4d
mustacheswap <83590343+mustacheswap@users.noreply.github.com>
2021-05-15 22:30:30 +0200

    Update Logo.tsx

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -9,16 +9,10 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-      <h1
-      style={{
-        fontFamily: "mustache",
-        textTransform: "uppercase",
-        fontSize: "14pt",
-      }}
-    >
-      Mustacheswap
-    </h1>
-  );
+    <Svg viewBox="0 0 410 52" {...props}>
+      <image width="410" height="52" href={isDark ? '/images/mustache/LogoTextNewDark.png' : '/images/mustache/LogoTextNewWhite.png'}/>
+    </Svg>
+  )
 };
 
 export default Logo;
